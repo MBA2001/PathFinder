7e42191d14509bd275453ad4461631bbbf30f3ba
mohamed Baher <bahermohamad13@gmail.com>
2020-10-07 21:22:41 +0200

    Updates

---
+++ precache-manifest.f2e297349abc69a2d3e5e337c8698040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058837336ab0f0cbb5b61de96f074d68",
+    "url": "/PathFinder/index.html"
+  },
+  {
+    "revision": "44dbbf81ab9c6f71a961",
+    "url": "/PathFinder/static/css/main.05fa15eb.chunk.css"
+  },
+  {
+    "revision": "ce92931ab194ab861102",
+    "url": "/PathFinder/static/js/2.7e05c31c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PathFinder/static/js/2.7e05c31c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44dbbf81ab9c6f71a961",
+    "url": "/PathFinder/static/js/main.9f7e5fde.chunk.js"
+  },
+  {
+    "revision": "1ef6d8c48263cd9f556e",
+    "url": "/PathFinder/static/js/runtime-main.2b1cd3a3.js"
+  }
+]);
